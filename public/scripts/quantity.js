588201309b3b0fd135d70b06365390a1ab00267c
kevinzhu2019 <kevinzhu.zyk@gmail.com>
2020-01-14 17:38:23 +0000

    Fixed the bug of Total amounts duplicate display every time Checkout button is clicked, remove the return object

--- public/scripts/quantity.js
+++ public/scripts/quantity.js
@@ -16,6 +16,15 @@ $(document).ready(function() {
   });
 
   $("div.order-total button.checkout-button").on("click", function() {
+    //Reset the "Total" amouts before we do anything.
+    let subTotalOnScreenLocation = $(".order-calculations").children("p")[0];
+    subTotalOnScreenLocation.innerText = "Subtotal:";
+    let taxesOnScreenValueLocation = $(".order-calculations").children("p")[1];
+    taxesOnScreenValueLocation.innerText = "Taxes:";
+    let totalOnScreenValueLocation = $(".order-calculations").children("p")[2];
+    totalOnScreenValueLocation.innerText = "Total";
+
+    //iterate the HTML with all divs named "food-item"
     let total = 0;
     $("section.order-food-interface div.food-item").each((index, element) => {
       let price = Number($(element).children(".price")[0].innerText.slice(1));
@@ -26,19 +35,20 @@ $(document).ready(function() {
     let tax = (total * 0.13).toFixed(2);
     let totalWithTax = Number(total) + Number(tax);
     let subTotalOnScreenValue = $(".order-calculations").children("p")[0].innerText;
-    let subTotalOnScreenLocation = $(".order-calculations").children("p")[0];
     subTotalOnScreenLocation.innerText = subTotalOnScreenValue + " $" + total;
+
+
     let taxesOnScreenValue = $(".order-calculations").children("p")[1].innerText;
-    let taxesOnScreenValueLocation = $(".order-calculations").children("p")[1];
     taxesOnScreenValueLocation.innerText = taxesOnScreenValue + " $" + tax;
+
+
     let totalOnScreenValue = $(".order-calculations").children("p")[2].innerText;
-    let totalOnScreenValueLocation = $(".order-calculations").children("p")[2];
     totalOnScreenValueLocation.innerText = totalOnScreenValue + " $" + totalWithTax;
     // console.log(subTotalOnScreen, taxesOnScreen, totalOnScreen);
-    return {
-      Subtotal: total,
-      Taxes: tax,
-      Total: totalWithTax
-    }
+    // return {
+    //   Subtotal: total,
+    //   Taxes: tax,
+    //   Total: totalWithTax
+    // }
   });
 });
